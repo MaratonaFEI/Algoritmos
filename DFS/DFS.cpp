7ee82a9509ab05c1eabec6bccea13a26c11bdcef
Kenzo <79611160+Kenzo-Sugai@users.noreply.github.com>
2022-09-14 14:57:21 -0300

    Create DFS.cpp

---
+++ DFS/DFS.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+
+using namespace std;
+
+vector<vector<int>> G; // Lista de Adjacencia
+vector<int> d; // Vetor de Distancias
+vector<int> cor; // Vetor de Cores
+vector<int> p; // Vetor de Pais
+
+stack<int> S; // Stack de Vertices
+int DFS(int i){
+
+    S.push(i);
+    d[i] = 0;
+    cor[i] = true;
+
+    while(not S.empty()){
+
+        int u = S.top();
+        S.pop();
+
+        for(int i = 0; i < G[u].size(); i++){
+            if(not cor[G[u][i]]){
+
+                cor[G[u][i]] = true;
+                d[G[u][i]] = d[u] + 1;
+                p[G[u][i]] = u;
+
+                S.push(G[u][i]);
+            }
+        }
+    }
+}
+
+int main(){
+
+    int
+    N, // Numero de Vertices
+    M, // Numero de Arestas
+    X, // Vertice X
+    Y; // Vertice Y
+
+    cin >> N >> M;
+
+    G.resize(N);
+
+    for(int i = 0; i < M; i++){
+        cin >> X >> Y;
+        G[X].push_back(Y);
+        G[Y].push_back(X);
+    }
+
+    d.resize(N, -1);
+    cor.resize(N, false);
+    p.resize(N, -1);
+
+    DFS(0);
+
+    cout << "Distancias: " << endl;
+    for(auto &x: d) cout << x << " ";
+    cout << endl;
+
+}
