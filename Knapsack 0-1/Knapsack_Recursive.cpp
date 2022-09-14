0ed7e789fd993fa19c7b0c1cd8695bdf00afe9d5
Kenzo <79611160+Kenzo-Sugai@users.noreply.github.com>
2022-09-14 15:42:04 -0300

    Create Knapsack_Recursive.cpp

---
+++ Knapsack 0-1/Knapsack_Recursive.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int tam;
+
+struct mochila {
+    int valor;
+    int peso;
+};
+
+vector<mochila> mochila;
+
+int Knapsack(int tamanho, int i){
+  if(tamanho == 0 or i == 0){
+      return 0;
+  }
+
+  if(mochila[i - 1].peso > tamanho) return Knapsack(tamanho, i - 1);
+    
+  else {
+      int use_item = mochila[i - 1].valor + Knapsack(tamanho - mochila[i - 1].peso, i - 1);
+      int no_use_item = Knapsack(tamanho, i - 1);
+      return max(use_item, no_use_item);
+  }
+}
+
+int main(){
+
+  int N, v, w;
+  cin >> N;
+  
+  cin >> tam;
+  
+  for(int i = 0; i < N; i++){
+    cin >> v >> w;
+    mochila.push_back({v, w});
+  }
+
+  cout << Knapsack(tam, N) << endl;
+
+}
