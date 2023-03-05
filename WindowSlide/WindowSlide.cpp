d64c54c489b630f8a49b07a8622fc02080e0a669
Kenzo <79611160+Kenzo-Sugai@users.noreply.github.com>
2023-03-04 23:30:32 -0300

    Create WindowSlide.cpp

---
+++ WindowSlide/WindowSlide.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+
+using namespace std;
+
+void windowSlide(string &word){
+
+    string temp;
+
+    for(int i = 1; i <= word.size(); i++){
+
+        for(int j = 0; j < word.size(); j++){
+
+            temp = "";
+
+            for(int k = 0; k < i && i + j <= word.size() && j + k < word.size(); k++){
+
+                cout << word[i + j];
+
+            }
+
+            cout << endl;
+
+        }
+
+    }
+
+}
+
+int main(){
+  
+  string word = "ABCDEF"
+  windowSlide(word);
+  /*
+  OUTPUT:
+
+  A
+  B
+  C
+  D
+  E
+  F
+  
+  AB
+  BC
+  CD
+  DE
+  EF
+  
+  ABC
+  BCD
+  CDE
+  DEF
+  
+  ABCD
+  BCDE
+  CDEF
+  
+  ABCDE
+  BCDEF
+  ABCDEF
+  
+  */
+  
+}
