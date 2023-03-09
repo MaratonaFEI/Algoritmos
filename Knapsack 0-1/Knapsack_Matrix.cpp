9de82b1a1128a87761854c638df5ce51f9c43c01
Kenzo <79611160+Kenzo-Sugai@users.noreply.github.com>
2023-03-09 19:36:42 -0300

    Update Knapsack_Matrix.cpp

--- Knapsack 0-1/Knapsack_Matrix.cpp
+++ Knapsack 0-1/Knapsack_Matrix.cpp
@@ -14,23 +14,23 @@ vector<conteudo> mochila; // Mochila com pesos e valores
 
 int Knapsack(int v_size, int tamanho){
 
-  vector<vector<int>> cashe(v_size + 1, vector<int>(tamanho + 1)); // Matriz de possibilidades da Mochila
+  vector<vector<int>> cache(v_size + 1, vector<int>(tamanho + 1)); // Matriz de possibilidades da Mochila
 
   for(int i = 0; i <= N; i++){
 
     for(int w = 0; w <= tamanho; w++){
 
-      if(i == 0 or w == 0) cashe[i][w] = 0;
+      if(i == 0 or w == 0) cache[i][w] = 0;
         
       else if(mochila[i - 1].peso <= w)
-        cashe[i][w] = max(mochila[i - 1].valor +
-        cashe[i - 1][w - mochila[i - 1].peso],
-        cashe[i - 1][w]);
+        cache[i][w] = max(mochila[i - 1].valor +
+        cache[i - 1][w - mochila[i - 1].peso],
+        cache[i - 1][w]);
         
-      else cashe[i][w] = cashe[i - 1][w];
+      else cache[i][w] = cache[i - 1][w];
     }
   }
-  return cashe[v_size][tamanho];
+  return cache[v_size][tamanho];
 }
 
 
