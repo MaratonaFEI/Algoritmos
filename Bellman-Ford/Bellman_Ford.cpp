bf3b40f9d17d9a20b53f4d5000f0d87890da5a3e
Kenzo <79611160+Kenzo-Sugai@users.noreply.github.com>
2023-04-25 22:32:08 -0300

    Update Bellman_Ford.cpp

--- Bellman-Ford/Bellman_Ford.cpp
+++ Bellman-Ford/Bellman_Ford.cpp
@@ -10,7 +10,7 @@ struct vertice {
 
 void init(std::vector<vertice> &G, int s){
 
-    for(vertice u : G){
+    for(vertice &u : G){
         u.d = INT_MAX;
         u.p = -1;
     }
@@ -21,7 +21,7 @@ void init(std::vector<vertice> &G, int s){
 
 void relax(std::vector<vertice> &G, std::vector<std::vector<int>> &w, int u, int v){
 
-    if(G[v].d > G[u].d + w[u][v]){
+    if(G[u].d != INT_MAX && G[v].d > G[u].d + w[u][v]){
         G[v].d = G[u].d + w[u][v];
         G[v].p = u;
     }
@@ -31,29 +31,45 @@ void relax(std::vector<vertice> &G, std::vector<std::vector<int>> &w, int u, int
 bool bellman_ford(std::vector<vertice> &G, std::vector<std::vector<int>> &w, int s){
 
     init(G, s);
-    std::cout << "init complete" << std::endl;
-    for(int i = 1; i < G.size(); i++){
 
-        for(int v : G[i].E){
+    for (int &v: G[s].E) {
 
-            relax(G, w, i, v);
-
-        }
+        relax(G, w, s, v);
+        relax(G, w, v, s);
+//        for(vertice &u : G){
+//            std::cout << u.d << " ";
+//        }
+//        std::cout << std::endl;
 
     }
-    std::cout << "w set complete" << std::endl;
+
+    for(int i = 1; i < G.size(); i++){
+        //for (int u = 0; u < G.size(); u++) {
+            for (int &v: G[i].E) {
+                //std::cout << i << " " << v << std::endl;
+                relax(G, w, i, v);
+                relax(G, w, v, i);
+//                for(vertice &u : G){
+//                    std::cout << u.d << " ";
+//                }
+//                std::cout << std::endl;
+
+            }
+        //}
+    }
+
     for(int i = 0; i < G.size(); i++){
 
         for(int v : G[i].E){
 
-           if(G[v].d > G[i].d + w[i][v]) return false;
+            if(G[v].d > G[i].d + w[i][v]) return false;
 
         }
 
     }
-    std::cout << "cyclic complete" << std::endl;
 
     return true;
+
 
 }
 
@@ -74,26 +90,22 @@ int main() {
         X -= 'a';
         Y -= 'a';
 
-        //std::cout<< int(X) << " " << int(Y) << " " << Z << std::endl;
-
-
         G[X].E.push_back(Y);
         G[Y].E.push_back(X);
         w[X][Y] = Z;
         w[Y][X] = Z;
 
-        //std::cout << i << std::endl;
-
     }
 
-    std::cout << "input complete" << std::endl;
+    for (int i = 0; i < V; i++) {
+        std::cout << "=- " << i << " -=" << std::endl;
+        bellman_ford(G, w, i);
 
-    bellman_ford(G, w, 0);
+        char it = 'a';
 
-    char it = 'a';
-
-    for(vertice u : G){
-        std::cout << it++ << ": " << u.d << std::endl;
+        for(vertice &u : G){
+            std::cout << it++ << ": " << u.d << std::endl;
+        }
     }
 
     return 0;
